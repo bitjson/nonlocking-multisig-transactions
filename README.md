3cd86658926f28eaf0679d39a08d32927874f5c3
Jason Dreyzehner <jason@bitpay.com>
2015-07-17 13:45:07 -0400

    exploring

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Nonlocking Transactions for Copay
 =================================
 
+**(WIP)**
+
 This script simulates a Copay wallet with transactions that lock only the funds necessary to complete outstanding transaction proposals.
--- nonlocking.js
+++ nonlocking.js
@@ -1,21 +1,28 @@
 //simulate Copay multisig wallet
-var wallet = [];
+var walletState = [];
+
+var unsignedTransactions = [];
+var broadcastedTransactions = [];
+
 var proposals = [];
-var lockedUTXOs = [];
-var spent = [];
-var fees = [];
+var potentialSplits = [];
+
+var feesSpent = 0;
+var otherWallets = [];
+
+var debug = true;
 
 //start simulation
 receive(100000);
-status('=========== received 100000 ===========');
+// status('=========== received 100000 ===========');
 var txA = proposeSpend(500);
 var txB = proposeSpend(1000);
-var txC = proposeSpend(50);
-status('=========== TXs proposed ===========');
-reject(txA);
-reject(txC);
-accept(txB);
-status('=========== TXs handled ===========');
+// var txC = proposeSpend(50);
+// // status('=========== TXs proposed ===========');
+// accept(txA);
+// reject(txC);
+// accept(txB);
+// status('=========== final ===========');
 
 
 // end simulation
@@ -26,75 +33,136 @@ status('=========== TXs handled ===========');
 // wallet logic
 
 function receive(amount) {
-  utxoId = wallet.length;
-  wallet.push({
-    utxoId: utxoId,
+  var trasaction = {
+    utxoId: walletState.length,
     amount: amount
-  });
+  };
+  walletState.push(trasaction);
+  debugMsg('received transaction: ', trasaction);
 }
 
-function getBalance() {
-  var balance = 0;
-  for (var a = 0; a < wallet.length; a++) {
-    balance += wallet[a].amount;
+function getLockedUtxoIds() {
+  var lockedIds = [];
+  for (var a = 0; a < potentialSplits.length; a++) {
+    lockedIds.push(potentialSplits[a].lock);
   }
+  return lockedIds;
+}
+
+function getUnlockedBalance() {
+  var balance = 0;
+  var lockedIds = getLockedUtxoIds();
+  // get unlocked utxos
+  for (var a = 0; a < walletState.length; a++) {
+    if (lockedIds.indexOf(walletState[a].utxoId) === -1) {
+      balance += walletState[a].amount;
+    }
+  }
+  for (var a = 0; a < potentialSplits.length; a++) {
+    balance += potentialSplits[a].changeUtxoAmount;
+  }
+  debugMsg('Unlocked wallet balance: ', balance);
   return balance;
 }
 
 function selectUTXOs(amount) {
-  var inputs = [];
+  debugMsg('selecting UTXOs for amount: ', amount);
+  var inputIds = [];
   var done = false;
   var remaining = amount;
-
-  var selectUTXO = function (utxo){
-    inputs.push(utxo.utxoId);
-    remaining -= utxo.amount;
-    if(remaining <= 0) {
-      done = true;
+  var lockedIds = getLockedUtxoIds();
+  var utxosFromSplits = false;
+  var filterUtxos = function(utxo) {
+    // filter out utxos already used
+    if (inputIds.indexOf(utxo.utxoId) !== -1) {
+      return false;
     }
-  }
+    // filter out utxos locked by other proposals
+    return lockedIds.indexOf(utxo.utxoId) === -1;
+  };
 
-  while(!done){
+  while (remaining >= 0) {
+    var tempWallet;
+    if (utxosFromSplits) {
+      // reuse utxos from splits in selection
+      tempWallet = tempWallet.concat(utxosFromSplits);
+    } else {
+      tempWallet = walletState.filter(filterUtxos);
+      if (tempWallet.length === 0) {
+        // need to use utxos from splits
+        utxosFromSplits = [];
+        for (var s = 0; s < potentialSplits.length; s++) {
+          utxosFromSplits.push({
+            utxoId: 's' + potentialSplits[s].splitId,
+            amount: potentialSplits[s].changeUtxoAmount
+          });
+          tempWallet = tempWallet.concat(utxosFromSplits);
+        }
+      }
+    }
     var placeholder = {
-      utxoId: 'placeholder',
+      utxoId: 'amount needed',
       amount: remaining
     };
-
-    var tempWallet = wallet
+    tempWallet = tempWallet
       .concat(placeholder)
       .sort(descending('amount'));
-
+    debugMsg('Sorted tempWallet: ');
+    debugMsg(tempWallet);
     var placeholderIndex = tempWallet.indexOf(placeholder);
-
     // try to get smallest utxo which is greater than `remaining`,
     // if `remaining` is greater than all utxos, get the largest utxo
-
-    if(placeholderIndex > 0){
-      selectUTXO(tempWallet[placeholderIndex - 1]);
+    var selectedUtxo;
+    if (placeholderIndex > 0) {
+      selectedUtxo = tempWallet[placeholderIndex - 1];
     } else {
-      selectUTXO(tempWallet[1]);
+      selectedUtxo = tempWallet[1];
     }
+    inputIds.push(selectedUtxo.utxoId);
+    remaining -= selectedUtxo.amount;
   }
+  var change = remaining * -1;
+  debugMsg('selected UTXOs: ', inputIds);
+  debugMsg('    change: ', change);
   return {
-    inputs: inputs,
-    change: remaining * -1
+    inputIds: inputIds,
+    change: change
   };
 }
 
 function proposeSpend(amount) {
-  if (getBalance() <= amount) {
+  debugMsg('Proposing spend of: ', amount);
+  var walletBalance = getUnlockedBalance();
+  if (walletBalance <= amount) {
+    console.error('Insufficient unlocked funds. Wallet balance is: ', walletBalance, ' – Proposed amount is: ', amount);
     return false;
   }
+  debugMsg('Sufficient funds, proceeding...');
   var proposalId = proposals.length;
-
+  // select UTXOs
   var utxoSelectionResult = selectUTXOs(amount);
+  //fee fixed for simulation
+  var fee = 100;
 
-  //fixed for simulation
-  var fee = 1;
+  //TODO: split change, using as few inputs as possible from the first X inputs in result
+
+  var largestSelectedUtxo;
+
+  if(utxoSelectionResult.inputIds[0].toString().charAt(0) === 's'){
+    // largest UTXO is from split
+  }
+
+  for (var i = 0; i < walletState.length; i++) {
+    if (walletState[i].utxoId === utxoSelectionResult.inputIds[0]) {
+      largestSelectedUtxo = i;
+      break;
+    }
+  }
+  var splitUtxos = prepareSplit(largestSelectedUtxo, utxoSelectionResult.change - fee);
 
   proposals.push({
     id: proposalId,
-    inputs: utxoSelectionResult.inputs,
+    inputs: utxoSelectionResult.inputIds,
     output: amount,
     change: utxoSelectionResult.change - fee,
     fee: fee
@@ -102,11 +170,22 @@ function proposeSpend(amount) {
   return proposalId;
 }
 
-function splitUtxo(utxo, amount){
-  return {
-    newUtxo: {},
-    changeUtxo: {}
+
+// TODO: this function actually needs to combine as many inputs as necessary to
+// output a utxo for the proposal and the change it will produce
+
+function prepareSplit(utxo, change) {
+  debugMsg('Preparing split – change: ', change, ' from UTXO: ', utxo);
+  var split = {
+    splitId: potentialSplits.length,
+    lock: utxo,
+    newUtxoAmount: walletState[utxo].amount - change,
+    changeUtxoAmount: change
   };
+  potentialSplits.push(split);
+  debugMsg('Saved potential split:');
+  debugMsg(split);
+  return split;
 }
 
 function reject(proposalId) {
@@ -124,11 +203,23 @@ function accept(proposalId) {
 function status(message) {
   console.log(message);
   console.log("wallet:");
-  console.log(wallet.sort(descending('amount')));
+  console.log(walletState.sort(descending('amount')));
   console.log("proposals:");
-  console.log(proposals);
-  console.log("spent:");
-  console.log(spent);
+  console.log(proposals.sort(descending('output')));
+  console.log("potentialSplits:");
+  console.log(potentialSplits);
+  console.log("broadcastedTransactions:");
+  console.log(broadcastedTransactions);
+  console.log("feesSpent:");
+  console.log(feesSpent);
+  console.log("otherWallets:");
+  console.log(otherWallets.sort(descending('amount')));
+}
+
+function debugMsg() {
+  if (debug) {
+    console.log.apply(console, arguments);
+  }
 }
 
 function ascending(prop) {
@@ -140,12 +231,13 @@ function ascending(prop) {
     return 0;
   };
 }
+
 function descending(prop) {
   return function(a, b) {
     if (a[prop] < b[prop])
-      return -1;
-    if (a[prop] > b[prop])
       return 1;
+    if (a[prop] > b[prop])
+      return -1;
     return 0;
   };
 }
